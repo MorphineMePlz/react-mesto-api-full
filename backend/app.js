2753c9b37f20dcd36667b37271ea33c38edf846c
Азиз Гейдар <azizgeidar@Azizs-MacBook-Air.local>
2023-01-02 16:30:17 +0600

    fix: cors v4

--- backend/app.js
+++ backend/app.js
@@ -25,20 +25,21 @@ const { validateLogin } = require('./middlewares/validator');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-const options = {
-  origin: [
-    'http://localhost:3000',
-    'http://ageidar.nomoredomains.club',
-    'https://ageidar.nomoredomains.club',
-    'http://api.ageidar.nomoredomains.club',
-    'https://api.ageidar.nomoredomains.club',
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+const allowedCors = [
+  'http://localhost:3000',
+  'http://ageidar.nomoredomains.club',
+  'https://ageidar.nomoredomains.club',
+  'http://api.ageidar.nomoredomains.club',
+  'https://api.ageidar.nomoredomains.club',
+];
+
+const cosrOptions = {
+  origin: allowedCors,
+  optionSuccessStatus: 200,
   credentials: true,
 };
 
-app.use(cors(options));
+app.use(cors(cosrOptions));
 
 app.use(express.json());
 app.use(helmet());
